6fe9a4f7f297276ac85dd521872d099caa15f5f0
Tyler Doll <tyler@Tylers-MacBook-Pro-6.local>
2020-05-17 17:27:23 -0600

    fix: force dark mode always

--- ui/src/App.js
+++ ui/src/App.js
@@ -2,12 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 
-import {
-  ThemeProvider,
-  CSSReset,
-  ColorModeProvider,
-  useColorMode,
-} from "@chakra-ui/core";
+import { ThemeProvider, CSSReset, DarkMode } from "@chakra-ui/core";
 import { Tabs, TabList, TabPanels, TabPanel } from "@chakra-ui/core";
 
 import { FaList, FaPlus, FaHome, FaHistory } from "react-icons/fa";
@@ -21,16 +16,13 @@ import HistoryTab from "./components/tabs/History";
 import { setTabIndex } from "./data/actions/app";
 
 const App = (props) => {
-  const { toggleColorMode } = useColorMode();
-  toggleColorMode("dark");
-
   const { tabIndex, setTabIndex } = props;
 
   const handleTabsChange = (index) => setTabIndex(index);
 
   return (
     <ThemeProvider>
-      <ColorModeProvider>
+      <DarkMode>
         <CSSReset />
         <Tabs
           variant="soft-rounded"
@@ -42,44 +34,45 @@ const App = (props) => {
           index={tabIndex}
           onChange={handleTabsChange}
         >
-          <TabPanels
-            flex={1}
-            display="flex"
-            flexDirection="column"
-            p={4}
-            pb="80px"
-          >
-            <TabPanel>
-              <VisitorsTab />
-            </TabPanel>
-            <TabPanel>
-              <NewVisitorTab />
-            </TabPanel>
-            <TabPanel>
-              <ResidentTab />
-            </TabPanel>
-            <TabPanel>
-              <HistoryTab />
-            </TabPanel>
-          </TabPanels>
-
           <TabList
             bg="gray.700"
             height="64px"
             display="flex"
             position="fixed"
             p={4}
-            bottom={0}
+            top={0}
             left={0}
             right={0}
+            zIndex={100}
           >
             <IconTab label="Visitors" icon={FaList} color="green.200" />
             <IconTab label="Add visitor" icon={FaPlus} color="blue.200" />
             <IconTab label="Resident" icon={FaHome} color="red.200" />
             <IconTab label="History" icon={FaHistory} color="yellow.200" />
           </TabList>
+
+          <TabPanels
+            flex={1}
+            display="flex"
+            flexDirection="column"
+            p={4}
+            pt="80px"
+          >
+            <TabPanel py={4}>
+              <VisitorsTab />
+            </TabPanel>
+            <TabPanel py={4}>
+              <NewVisitorTab />
+            </TabPanel>
+            <TabPanel py={4}>
+              <ResidentTab />
+            </TabPanel>
+            <TabPanel py={4}>
+              <HistoryTab />
+            </TabPanel>
+          </TabPanels>
         </Tabs>
-      </ColorModeProvider>
+      </DarkMode>
     </ThemeProvider>
   );
 };
